e9f70d7d995a493ee7f700ec8c7d6d1987224ece
정구민 <jgm@paytalab.com>
2025-02-06 15:11:27 +0900

    tmp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask // openapi generator task
+
 
 plugins {
     id("org.springframework.boot") version "2.7.12"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
-    id("org.openapi.generator") version "6.6.0"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
+    id("org.openapi.generator") version "7.0.1" // openapi generator plugin
+//    id("org.openapi.generator") version "6.6.0" // openapi generator plugin
 }
 
 group = "com.example"
@@ -21,13 +24,113 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation ("org.springframework.boot:spring-boot-starter-validation")
 
-    // open api generator dependencies
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-    implementation("org.openapitools:jackson-databind-nullable:0.2.4")
-    implementation("jakarta.annotation:jakarta.annotation-api:2.1.1")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0") // openapi 를 위해 필수로 필요
 }
+
+sourceSets {
+    main {
+        java.srcDirs("$buildDir/generated")
+    }
+}
+
+// api, invoker, model이 위치할 패키지 경로
+val openApiPackages = listOf("openapi.api", "openapi.invoker", "openapi.model")
+
+val dirs = mapOf(
+    // 명세가 위치한 경로
+    "openapi-specific" to "$rootDir/src/main/resources/openapi-specific",
+    // 생성되는 코드들이 위치할 경로
+    "openApiGenerate" to "$buildDir/openapi"
+)
+
+// 명세를 기반으로 코드를 생성하는 task들을 저장한다.
+val generateOpenApiTasks: List<TaskProvider<GenerateTask>> = fileTree(dirs["openapi-specific"] ?: error("openapi-specific dir not found"))
+    .files
+    .filter { file -> (file.name.endsWith(".yaml") || file.name.endsWith(".yml")) }
+    .map { file -> createOpenApiGenerateTask(file.name) }
+
+fun createOpenApiGenerateTask(fileName: String): TaskProvider<GenerateTask> {
+    return tasks.register<GenerateTask>("openApiGenerate_$fileName") {
+        generatorName.set("spring")
+        inputSpec.set("${dirs["openapi-specific"]}/$fileName")
+        outputDir.set(dirs["openApiGenerate"] ?: error("Output directory not found"))
+        apiPackage.set(openApiPackages[0])
+        invokerPackage.set(openApiPackages[1])
+        modelPackage.set(openApiPackages[2])
+
+        // 다음 문서를 확인하여 적절한 옵션을 넣는다.
+        // https://openapi-generator.tech/docs/generators/spring
+        configOptions.set(
+            mapOf(
+                "dateLibrary" to "spring",
+                "useSpringBoot3" to "true",
+                "useTags" to "true",
+                "openApiNullable" to "false",
+                // API를 interface로 생성한다.
+                "interfaceOnly" to "true",
+//                "reactive" to "true" // WebFlux 설정
+            )
+        )
+    }
+}
+
+tasks.register("createOpenApi") {
+    doFirst {
+        println("Creating Code By OpenAPI...")
+    }
+    doLast {
+        println("OpenAPI Code created.")
+    }
+    // generateOpenApiTasks의 모든 Task에 의존한다.
+    dependsOn(generateOpenApiTasks)
+}
+
+tasks.register("moveGeneratedSources") {
+    doFirst {
+        println("Moving generated sources...")
+    }
+
+    doLast {
+        openApiPackages.forEach { packageName ->
+            val packagePath = packageName.replace(".", "/")
+            val originDir = file("${dirs["openApiGenerate"]}/src/main/java/$packagePath")
+            val destinationDir = file("$buildDir/generated/$packagePath")
+            copy {
+                from(originDir)
+                into(destinationDir)
+            }
+        }
+        println("Generated sources moved.")
+    }
+    // 해당 작업은 createOpenApi Task에 의존한다.
+    dependsOn("createOpenApi")
+}
+
+tasks.register("cleanGeneratedDirectory") {
+    doFirst {
+        println("Cleaning generated directory...")
+    }
+    doLast {
+        val openApiGenerateDir = file(dirs["openApiGenerate"] ?: error("openApiGenerate directory not found"))
+        if (openApiGenerateDir.exists()) {
+            openApiGenerateDir.deleteRecursively()
+            println("Directory [${openApiGenerateDir}] deleted.")
+        } else {
+            println("Directory [${openApiGenerateDir}] does not exist.")
+        }
+    }
+    // 해당 작업은 moveGeneratedSources에 의존한다.
+    dependsOn("moveGeneratedSources")
+}
+
+tasks.named("compileJava") {
+    // 컴파일 이전에 코드 생성 작업이 수행된다.
+    dependsOn("cleanGeneratedDirectory")
+}
+
+/* End Openapi generator setting */
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
@@ -38,33 +141,4 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-tasks {
-    compileKotlin {
-        dependsOn("openApiGenerate")
-    }
-}
-
-openApiGenerate {
-    generatorName.set("kotlin-spring")
-    inputSpec.set("$rootDir/src/main/resources/docs/openapi.yml")
-    outputDir.set("$buildDir/generated")
-    apiPackage.set("com.example.api")
-    modelPackage.set("com.example.model")
-    invokerPackage.set("com.example.invoker")
-    configOptions.set(
-        mapOf(
-            "dateLibrary" to "java8",
-            "interfaceOnly" to "true",
-        ),
-    )
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir("$buildDir/generated/src/main/kotlin")
-        }
-    }
 }
